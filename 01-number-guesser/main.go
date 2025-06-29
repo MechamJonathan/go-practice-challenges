65b3a10f9f36ffcdf1477cb8bd91bcb348193cd5
MechamJonathan <jonsmec95@gmail.com>
2025-06-29 05:49:44 -0600

    number guesser

--- 01-number-guesser/main.go
+++ 01-number-guesser/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Print("first challenge project!")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Welcome to Number Guesser!")
+	fmt.Println(("-------------------------"))
+	fmt.Println("> I've picked a random number (1-100). Guess it.")
+
+	num := generateRandomNumber()
+
+	for {
+		fmt.Print("-> ")
+		input, _ := reader.ReadString('\n')
+
+		input = strings.Replace(input, "\n", "", -1)
+		i, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Invalid Input: ", err)
+		}
+
+		if i < num {
+			fmt.Println("higher")
+		} else if i > num {
+			fmt.Println("lower")
+		} else {
+			fmt.Println("Nice... you guessed it correctly.")
+			fmt.Println("Want to play again? Y/N")
+			fmt.Print("-> ")
+
+			input, _ := reader.ReadString('\n')
+			input = strings.Replace(input, "\n", "", -1)
+
+			if input == "Y" {
+				num = generateRandomNumber()
+				fmt.Println("I picked a new number...")
+				continue
+			} else {
+				fmt.Println("see ya!")
+				break
+			}
+		}
+	}
+}
+
+func generateRandomNumber() int {
+	num := rand.Intn(100)
+	return num
 }
