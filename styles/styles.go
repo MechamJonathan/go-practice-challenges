0f251e8fa39d7f7705c2ac055a4cf4abaaef4e9e
MechamJonathan <jonsmec95@gmail.com>
2025-06-30 07:54:55 -0600

    undo changes

--- styles/styles.go
+++
@@ -1,9 +0,0 @@
-package styles
-
-import "github.com/charmbracelet/lipgloss"
-
-var (
-	result = lipgloss.Color("245")
-)
-
-var terminalStyle = lipgloss.NewStyle().Foreground(result).Bold(true).Align(lipgloss.Center)
